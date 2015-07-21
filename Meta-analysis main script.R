cb93ef6cee368a2e5afb23420d8018c6dcfd4d86
Gustav Nilsonne <gustav.nilsonne@ki.se>
2015-07-21 11:19:19 +0200

    Added multilevel model

--- Meta-analysis main script.R
+++ Meta-analysis main script.R
@@ -19,25 +19,25 @@ p <- 0.038
 t <- qt((1-p/2), df)
 r <- sqrt(t^2/(t^2 + df))
 res(r = r, n = n)
-data <- data.frame(author = "Grodstein", year = 2008, ri = r, ni = n, covariates = "age, educational attainment")
+data <- data.frame(study = "Grodstein08", group.in.study = "1.in.Grodstein08", author = "Grodstein", year = 2008, ri = r, ni = n, covariates = "age, educational attainment")
 
 # Wikgren 2012
 # We have r and n for both groups
-data <- rbind(data, data.frame(author = "Wikgren_e3e3", year = 2012, ri = -0.519, ni = 29, covariates = "age, body size"))
-data <- rbind(data, data.frame(author = "Wikgren_e4", year= 2012, ri = 0.134, ni = 28, covariates = "age, body size"))
+data <- rbind(data, data.frame(study = "Wikgren12", group.in.study = "1.in.Wikgren12", author = "Wikgren_e3e3", year = 2012, ri = -0.519, ni = 29, covariates = "age, body size"))
+data <- rbind(data, data.frame(study = "Wikgren12", group.in.study = "2.in.Wikgren12", author = "Wikgren_e4", year= 2012, ri = 0.134, ni = 28, covariates = "age, body size"))
 
 # King 2014
 # We have r2 and n
-data <- rbind(data, data.frame(author = "King", year = 2014, ri = sqrt(0.0091), ni = 1960, covariates = "age, sex, race/ethnicity"))
+data <- rbind(data, data.frame(study = "King14", group.in.study = "1.in.King14", author = "King", year = 2014, ri = sqrt(0.0091), ni = 1960, covariates = "age, sex, race/ethnicity"))
 
 # Wolkowitz 2015
 # We have r and n for both groups
-data <- rbind(data, data.frame(author = "Wolkowitz_MDD", year = 2015, ri = 0.05, ni = 19, covariates = "age, sex"))
-data <- rbind(data, data.frame(author = "Wolkowitz_control", year = 2015, ri = 0.35, ni = 17, covariates = "age, sex"))
+data <- rbind(data, data.frame(study = "Wolkowitz15", group.in.study = "1.in.Wolkowitz15", author = "Wolkowitz_MDD", year = 2015, ri = 0.05, ni = 19, covariates = "age, sex"))
+data <- rbind(data, data.frame(study = "Wolkowitz15", group.in.study = "2.in.Wolkowitz15", author = "Wolkowitz_control", year = 2015, ri = 0.35, ni = 17, covariates = "age, sex"))
 
 # Jacobs 2015
 # We have r2 and n
-data <- rbind(data, data.frame(author = "Jacobs", year = 2015, ri = sqrt(0.16), ni = 28, covariates = "age, education, BMI"))
+data <- rbind(data, data.frame(study = "Jacobs15", group.in.study = "1.in.Jacobs15", author = "Jacobs", year = 2015, ri = sqrt(0.16), ni = 28, covariates = "age, education, BMI"))
 
 # WRITE TABLE TO LATEX
 print(xtable(data))
@@ -45,14 +45,14 @@ print(xtable(data))
 # PERFORM META-ANALYSIS
 
 # Perform analysis
-res <- rma(ri = ri, ni = ni, measure = "COR", slab=paste(author, year, sep=" "), data = data)
-res
+#res <- rma(ri = ri, ni = ni, measure = "COR", slab=paste(author, year, sep=" "), data = data)
+#res
 
 # Make forest plot
-forest(res)
+#forest(res)
 
 # Make trim-and-fill analysis and show funnel plot
-taf <- trimfill(res)
+taf <- trimfill(res.ml)
 taf
 funnel(taf)
 
@@ -90,6 +90,16 @@ res3
 res4 <- rma(ri = ri, ni = ni, measure = "COR", slab=paste(author, year, sep=" "), data = data, mods = sex_f_frac)
 res4
 
+# PERFORM MULTILEVEL ANALYSIS
+# Since the dataset contains effects nested within studies, multilevel modelling is more appropriate in theory. 
+# Because the rma.mv function yields an object for which trim-and-fill analyses cannot be performed, we add this just to verify that this alternate specification gives similar results to the main analysis above.
+data <- data.frame(data, escalc(ri = data$ri, ni = data$ni, measure = "COR"))
+res.ml <- rma.mv(yi, vi, random = list(~ 1 | study, ~ 1 | group.in.study), slab=paste(author, year, sep=" "), data = data)
+summary.rma(res.ml)
+forest(res.ml)
+profile(res.ml, sigma2=1)
+profile(res.ml, sigma2=2)
+
 # ESTIMATE OBSERVED POWER
 pwr.r.test(n = 30, r = 0.12)
 pwr.r.test(n = 30, r = 0.23)
@@ -97,7 +107,7 @@ pwr.r.test(n = 30, r = 0.23)
 # MAKE FIGURES FOR PUBLICATION
 
 pdf("figure1.pdf")
-forest(res, alim = c(-0.8, 0.8), digits = c(2, 1), xlab = "r", ylim=c(-2.5,10), cex = 1.2, steps = 3)
+forest(res.ml, alim = c(-0.8, 0.8), digits = c(2, 1), xlab = "r", ylim=c(-2.5,10), cex = 1.2, steps = 3)
 addpoly(res2, row=-2, mlab="RE Model, Wikgren 2012 excluded", cex = 1.2)
 dev.off()
 
